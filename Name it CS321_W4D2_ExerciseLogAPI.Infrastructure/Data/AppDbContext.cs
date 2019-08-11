4f387100adf00ee9191977e52558e8e326ac8ee3
ck1962 <ckeslin@gmail.com>
2019-08-11 15:44:57 -0500

    initial create

---
+++ Name it CS321_W4D2_ExerciseLogAPI.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Name_it_CS321_W4D2_ExerciseLogAPI.Infrastructure.Data
+{
+    class AppDbContext : DbContext
+    {
+    }
+}
---
+++ Name it CS321_W4D2_ExerciseLogAPI.Infrastructure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Name_it_CS321_W4D2_ExerciseLogAPI.Infrastructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
